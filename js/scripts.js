073fab75cf987ee0d136225b1fbfb0facc8c7240
Bartosz Wojtaś <el.klides@op.pl>
2018-02-20 14:12:07 +0100

    Change scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
 var buttonElem = document.getElementById('new');
 
 function Phone(brand, price, color, lenght, width) {
-	this.brand = brand;
-	this.price = price;
-	this.color = color;
-	this.length = lenght;
-	this.width = width;
+  this.brand = brand;
+  this.price = price;
+  this.color = color;
+  this.length = lenght;
+  this.width = width;
 };
 
 Phone.prototype.printInfo = function() {
-	console.log('The phone brand is (' + this.brand + '), color is (' + this.color + ') and the price is (' + this.price + ').');
+  console.log('The phone brand is (' + this.brand + '), color is (' + this.color + ') and the price is (' + this.price + ').');
 };
 
 Phone.prototype.show = function() {
-	var itemList = document.getElementById('list');
-    var newElem = document.createElement('p');
-    newElem.innerHTML = this.brand;
-	console.log(newElem);
-    itemList.appendChild(newElem);
+  var itemList = document.getElementById('list');
+  var newElem = document.createElement('p');
+  newElem.innerHTML = this.brand;
+  console.log(newElem);
+  itemList.appendChild(newElem);
 };
 
-var SamsungGalaxyS6 = new Phone("Samsung", 1500, "pink", 154, 72);
-var iPhone6s = new Phone("Apple", 2000, "black", 145, 68);
-var OnePlusOne = new Phone("OnePlus", 1300, "silver", 149, 42);
+var SamsungGalaxyS6 = new Phone('Samsung', 1500, 'pink', 154, 72);
+var iPhone6s = new Phone('Apple', 2000, 'black', 145, 68);
+var OnePlusOne = new Phone('OnePlus', 1300, 'silver', 149, 42);
 
 iPhone6s.printInfo();
 SamsungGalaxyS6.printInfo();
@@ -32,11 +32,11 @@ SamsungGalaxyS6.show();
 OnePlusOne.show();
 
 buttonElem.addEventListener('click', function () {
-	newBrand = prompt('Please enter telephone brand', 'brand');
-	newPrice = prompt('Please enter telephone price', 'price');
-	newColor = prompt('Please enter telephone color', 'color');
-	newLenght = prompt('Please enter telephone lenght', 'lenght');
-	newWidth = prompt('Please enter telephone width', 'width');
-	var newPhone = new Phone(newBrand, newPrice, newColor, newLenght, newWidth);
-	newPhone.show()
+  newBrand = prompt('Please enter telephone brand', 'brand');
+  newPrice = prompt('Please enter telephone price', 'price');
+  newColor = prompt('Please enter telephone color', 'color');
+  newLenght = prompt('Please enter telephone lenght', 'lenght');
+  newWidth = prompt('Please enter telephone width', 'width');
+  var newPhone = new Phone(newBrand, newPrice, newColor, newLenght, newWidth);
+  newPhone.show();
 });
